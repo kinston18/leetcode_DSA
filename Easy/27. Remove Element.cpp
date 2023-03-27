120952b60677ac66e88c5a8acf9055da46949bb4
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-03-27 23:52:06 +0530

    Update 27. Remove Element.cpp

--- Easy/27. Remove Element.cpp
+++ Easy/27. Remove Element.cpp
@@ -10,3 +10,21 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 
 
 */
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+       vector<int> ::iterator it ;
+           it =nums.begin();
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]==val){
+                    nums.erase(it);
+                        it--;
+                    i--;
+                }
+                it++;
+            }
+        return nums.size();
+    }
+};
