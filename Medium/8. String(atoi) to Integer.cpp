7cbbf0b60232fc296f6084d9b0ad2ea9bc270c84
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-03-28 21:58:47 +0530

    Create 8. String(atoi) to Integer.cpp

---
+++ Medium/8. String(atoi) to Integer.cpp
@@ -0,0 +1,15 @@
+/*
+
+Input: s = "   -42"
+Output: -42
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-231, 231 - 1], the final result is -42.
+
+*/
