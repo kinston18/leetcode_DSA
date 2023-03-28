4f7a119a471fa7979a1ea7651afca44d92490b7d
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-03-28 21:36:22 +0530

    Update and rename Binary_Search.cpp to 704. Binary Search.cpp

--- Easy/704. Binary Search.cpp
+++ Easy/704. Binary Search.cpp
@@ -1,3 +1,13 @@
+/*
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+*/
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
