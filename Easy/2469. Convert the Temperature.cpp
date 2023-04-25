05c751e5dba6645672f9fc0a9ffbed6b1bddb5f7
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-04-25 19:55:13 +0530

    Update 2469. Convert the Temperature.cpp

--- Easy/2469. Convert the Temperature.cpp
+++ Easy/2469. Convert the Temperature.cpp
@@ -1,3 +1,17 @@
+
+
+/*
+
+
+Example 1:
+
+Input: celsius = 36.50
+Output: [309.65000,97.70000]
+Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+*/
+
+
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
