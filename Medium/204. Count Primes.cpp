c5fae6d16ac301332445bae12cb5b513ff2c8f82
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-07-05 00:24:32 +0530

    Create 204. Count Primes.cpp

---
+++ Medium/204. Count Primes.cpp
@@ -0,0 +1,11 @@
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+*/
