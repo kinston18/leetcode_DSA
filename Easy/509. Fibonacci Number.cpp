43e3bcbeb05237fe6be833e782027e6e736a18ee
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-07-07 17:43:56 +0530

    Create 509. Fibonacci Number.cpp

---
+++ Easy/509. Fibonacci Number.cpp
@@ -0,0 +1,26 @@
+/* Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+  */
+
+
+
+class Solution {
+public:
+     int fib(int n) {
+        if (n < 2)
+        return n;
+
+        int* memo = new int[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+        memo[i] = memo[i-1] + memo[i-2];
+        }
+
+        return memo[n];
+    }
+};
