a7ef380555c9ae990d2c72319a205e3d4d61cd69
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-07-09 00:00:27 +0530

    Update  1480. Running Sum of 1d Array.cpp

--- Easy/ 1480. Running Sum of 1d Array.cpp
+++ Easy/ 1480. Running Sum of 1d Array.cpp
@@ -11,7 +11,7 @@ Return the running sum of nums.
 
 Example 1:
 
-Input: nums = [1,2,3,4]
+Input: nums = [1,2,3,4] 
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
