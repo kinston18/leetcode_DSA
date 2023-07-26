555652faf8179dfb045cd638ec46c1eb7c17cdef
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-07-27 01:09:56 +0530

    Create 70. Climbing Stairs.cpp

---
+++ Easy/70. Climbing Stairs.cpp
@@ -0,0 +1,30 @@
+/*
+  You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+  */
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0)
+            return 1;
+        if(n==1 | n==2)
+            return n;
+        else 
+            int v=climbStairs(n-1)+climbStairs(n-2);
+    int step[n]=v;
+        return v;
+    }
+};
