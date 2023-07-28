85f575262e6ab011242d9b40f8b5abb1a1efd81b
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-07-29 00:28:31 +0530

    Update 509. Fibonacci Number.cpp

--- Easy/509. Fibonacci Number.cpp
+++ Easy/509. Fibonacci Number.cpp
@@ -6,6 +6,21 @@ Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
   */
 
 
+class Solution {
+public:
+    int fib(int n) {
+        //base case
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        int ans = fib(n-1) + fib (n-2);
+        return ans;
+    }
+};
+
+
+
 
 class Solution {
 public:
