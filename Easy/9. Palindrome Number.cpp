044a3c675c2fd9ea3df73efbf62ba32391efeec9
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-09-03 02:27:27 +0530

    Update 9. Palindrome Number.cpp

--- Easy/9. Palindrome Number.cpp
+++ Easy/9. Palindrome Number.cpp
@@ -1,1 +1,26 @@
+/*
+  Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
 
+*/
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        
+        long long reversed = 0;
+        long long temp = x;
+        
+        while(temp != 0){
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /=10;
+        }
+        return (reversed == x);
+    }
+};
