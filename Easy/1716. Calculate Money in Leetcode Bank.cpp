dfea6621478e41469a5182cdf7903804817c4470
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-12-06 23:46:33 +0530

    Update 1716. Calculate Money in Leetcode Bank.cpp

--- Easy/1716. Calculate Money in Leetcode Bank.cpp
+++ Easy/1716. Calculate Money in Leetcode Bank.cpp
@@ -31,3 +31,21 @@ public:
         return ans;
     }
 };
+
+
+
+
+//alternate solution:
+
+class Solution {
+public:
+    int totalMoney(int n) {
+        int total=0;
+        for(int day=0;day<n;day++)
+        {
+            total+=(day/7+1)+(day%7);
+        }
+        return total;
+        
+    }
+};
