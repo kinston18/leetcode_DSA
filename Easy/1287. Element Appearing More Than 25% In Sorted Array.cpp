f37341c10395864daf99cfba7f800ee5d0235335
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-12-11 23:47:27 +0530

    Update and rename d to 1287. Element Appearing More Than 25% In Sorted Array.cpp

--- Easy/1287. Element Appearing More Than 25% In Sorted Array.cpp
+++ Easy/1287. Element Appearing More Than 25% In Sorted Array.cpp
@@ -1,3 +1,14 @@
+/*
+    Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+    */
+
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
