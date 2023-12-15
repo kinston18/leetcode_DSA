402c856f363a11cde7976df43459d55c4abc1e5c
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-12-15 17:59:05 +0530

    Update 1436. Destination City.cpp

--- Easy/1436. Destination City.cpp
+++ Easy/1436. Destination City.cpp
@@ -7,23 +7,35 @@ Output: "Sao Paulo"
 Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
 
   */
+
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        for(int i =0;i<paths.size();i++){
+        // Loop through each path in the list
+        for (int i = 0; i < paths.size(); i++) {
+            // Pick the destination city from the current path
             string candidate = paths[i][1];
+
+            // Assume this city is good (for now)
             bool good = true;
-            
-            for(int j=0;j<paths.size();j++){
-                if(paths[j][0]==candidate){
+
+            // Check if this city is also a starting city in other paths
+            for (int j = 0; j < paths.size(); j++) {
+                // If the city is found as a starting city, it's not good
+                if (paths[j][0] == candidate) {
                     good = false;
+                    // No need to check further, we found it
                     break;
                 }
             }
-            if(good){
+
+            // If the city is good (not a starting city in other paths), return it
+            if (good) {
                 return candidate;
             }
         }
+
+        // If no good city is found, return an empty string
         return "";
     }
 };
