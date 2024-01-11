69775049d8bad018037d12b25432800b0cf8ec44
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-01-11 17:45:11 +0530

    Create 1. Two Sum.cpp

---
+++ Easy/1. Two Sum.cpp
@@ -0,0 +1,17 @@
+/*
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        target=nums[0]+nums[1];
+    }
+    return target;
+};
