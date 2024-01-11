5f34deb84dee56a4d01c8bea1cb771119388e98b
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-01-11 23:50:23 +0530

    Update 1046. Last Stone Weight.cpp

--- Easy/1046. Last Stone Weight.cpp
+++ Easy/1046. Last Stone Weight.cpp
@@ -9,8 +9,6 @@ At the end of the game, there is at most one stone left.
 
 Return the weight of the last remaining stone. If there are no stones left, return 0.
 
- 
-
 Example 1:
 
 Input: stones = [2,7,4,1,8,1]
@@ -22,3 +20,31 @@ we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
 we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
 */
 
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+         make_heap(stones.begin(),stones.end());
+        
+        while(stones.size() > 1)
+        {
+            pop_heap(stones.begin(),stones.end());   //push the max stone to the top
+            int x = stones.back();  // for largest stone
+            stones.pop_back();  //pop the max 
+            
+            pop_heap(stones.begin(),stones.end()); //push the second max stone to the top
+            int y = stones.back();  //for 2nd largest stone
+            stones.pop_back();    //pop the 2nd max
+            
+            int diff = abs(x-y);
+            if(diff>0){
+                stones.push_back(diff);
+                push_heap(stones.begin(),stones.end());
+            }
+        }
+        
+        if(stones.size()==1 )
+            return stones[0];
+        
+        return 0;
+    }
+};
