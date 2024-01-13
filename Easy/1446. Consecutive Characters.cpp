51e2f8de7b385b2927c98611e49cc55ce4dfade0
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-01-13 13:58:04 +0530

    Create 1446. Consecutive Characters.cpp

---
+++ Easy/1446. Consecutive Characters.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+
+  int maxPower(string s){
+    int maxi=1;
+    int cnt=1;
+
+    for(int i=1;i<s.length();i++){
+if(s[i]==s[i+1]){
+cnt++;
+}
+else 
+cnt=1;
+
+maxi=max(maxi,cnt);
+    }
+    return maxi;
+  }
+};
