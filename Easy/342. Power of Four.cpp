b7d3aae263441cdf5be184964d0a16f9a69f7007
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-01-14 04:00:35 +0530

    Create 342. Power of Four.cpp

---
+++ Easy/342. Power of Four.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0 || n==-1)
+            return false;
+        while(n%4==0)
+            n=n/4;
+        
+        return n==1;
+    }
+};
