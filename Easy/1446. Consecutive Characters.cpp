735e84919622327ff6e07da11a1d790c652712be
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-01-13 13:58:25 +0530

    Update 1446. Consecutive Characters.cpp

--- Easy/1446. Consecutive Characters.cpp
+++ Easy/1446. Consecutive Characters.cpp
@@ -1,19 +1,18 @@
-class Solution{
+class Solution {
 public:
-
-  int maxPower(string s){
-    int maxi=1;
-    int cnt=1;
-
-    for(int i=1;i<s.length();i++){
-if(s[i]==s[i+1]){
-cnt++;
-}
-else 
-cnt=1;
-
-maxi=max(maxi,cnt);
+    int maxPower(string s) {
+        int maxi=1;
+        int cnt=1;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]==s[i+1]){
+                cnt++;
+            }
+            else
+                cnt=1;
+            
+            maxi=max(cnt,maxi);
+        }
+        return maxi;
     }
-    return maxi;
-  }
 };
