8111d20e9a2a0e37bebe70130c5cbc51f13b9b92
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-01-14 03:45:59 +0530

    Update 136. Single Number.cpp

--- Easy/136. Single Number.cpp
+++ Easy/136. Single Number.cpp
@@ -1,1 +1,21 @@
+/*
+Assume the given array is: [4,1,2,1,2]
+XOR of all elements = 4^1^2^1^2
+      = 4 ^ (1^1) ^ (2^2)
+      = 4 ^ 0 ^ 0 = 4^0 = 4
+Hence, 4 is the single element in the array.
 
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        
+        int xorr = 0;
+        for(int i=0;i<n;i++){
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+};
