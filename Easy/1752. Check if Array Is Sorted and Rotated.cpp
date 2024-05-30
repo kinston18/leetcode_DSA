3662d23bd443d5c96cf7cc0807b7e327ef7efc86
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2024-05-30 13:04:53 +0530

    Update 1752. Check if Array Is Sorted and Rotated.cpp

--- Easy/1752. Check if Array Is Sorted and Rotated.cpp
+++ Easy/1752. Check if Array Is Sorted and Rotated.cpp
@@ -16,3 +16,25 @@ Explanation: [1,2,3,4,5] is the original sorted array.
 You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
 
 */
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size()-1;
+        
+        int count =0;
+        if(nums[n]>nums[0]){
+            count++;
+        }
+        
+        for(int i=1;i<=n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if(count<=1)  // if the count is more than 1 that means there are more than 2 values which is greater than there previous value in that case the array is not  sorted and rotated
+            return true;
+        
+        return false;
+    }
+};
